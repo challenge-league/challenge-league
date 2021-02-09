7a888d31d27c1e4195a0b51ccaee0efec73532b1
Dmitry Kozlov <dmitry.f.kozlov@gmail.com>
2021-02-09 05:51:06 +0300

    v0.1.0

--- go-dev.sh
+++ go-dev.sh
@@ -1,5 +1,5 @@
 set -euxo pipefail
 bash build.sh
 bash docker-build-dev.sh
-helm upgrade dataleague ./dataleague --set-string timestamp=$(date +%s)
+helm upgrade dataleague ./dataleague -f /dataleague/values-dev.yaml --set-string timestamp=$(date +%s)
 bash wait.sh
